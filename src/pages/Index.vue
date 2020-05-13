8e7192362e659c02e3d46f219ded9fea11793984
fsmaxdev <fsmaxdev@yandex.ru>
2020-05-13 15:53:10 +0500

    Start

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <h1>Сайт в разработке!</h1>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'MePro'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+h1 {
+  text-align: center;
+  align: center;
+}
+</style>
